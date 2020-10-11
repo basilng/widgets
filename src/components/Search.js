61ddc95bd83ea794ac3879a7c8a7bc040f524af6
Basil N G <basilng.bng@gmail.com>
2020-10-12 04:47:55 +0530

    useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  //called only once at the inital render
+  useEffect(() => {
+    console.log("I only run once");
+  }, []);
+  //call every time when the component rerender
+  useEffect(() => {
+    console.log("I only run once");
+  });
+  //call every time when the compoent rerender and eaither of the state chagne
+  useEffect(() => {
+    console.log("I only run once");
+  }, [ere, term2, tem3]);
 
   return (
     <div>
